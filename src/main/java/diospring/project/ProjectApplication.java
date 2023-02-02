78f85864ea9cb9027554e01c31d76981ef2bc1be
William <109118303+Ww1ll@users.noreply.github.com>
2023-02-02 10:05:51 -0300

    Add files via upload

---
+++ src/main/java/diospring/project/ProjectApplication.java
@@ -0,0 +1,16 @@
+package diospring.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
